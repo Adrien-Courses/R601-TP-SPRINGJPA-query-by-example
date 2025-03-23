fdebe06349419ffc805fbde119341e70cd0be2a4
adrien1212 <batman.caubel@laposte.net>
2025-03-23 16:50:53 +0100

    final

--- src/main/java/fr/adriencaubel/controller/StudentController.java
+++ src/main/java/fr/adriencaubel/controller/StudentController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import fr.adriencaubel.entity.Student;
@@ -19,7 +20,14 @@ public class StudentController {
 	
 	@GetMapping("/search")
     public List<Student> searchStudents(
+        @RequestParam(required = false) String firstName,
+        @RequestParam(required = false) String lastName,
+        @RequestParam(required = false) String lastNameStartsWith,
+        @RequestParam(required = false) Integer year,
+        @RequestParam(required = false) Double gpaMin,
+        @RequestParam(required = false) String city,
+        @RequestParam(required = false) String universityName
     ) {
-        return null;
+        return studentService.searchStudents(firstName, lastName, lastNameStartsWith, year, city, universityName);
     }
 }
--- src/main/java/fr/adriencaubel/service/StudentService.java
+++ src/main/java/fr/adriencaubel/service/StudentService.java
@@ -3,9 +3,12 @@ package fr.adriencaubel.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.stereotype.Service;
 
 import fr.adriencaubel.entity.Student;
+import fr.adriencaubel.entity.University;
 import fr.adriencaubel.repository.StudentRepository;
 
 @Service
@@ -14,7 +17,32 @@ public class StudentService {
 	@Autowired
 	private StudentRepository studentRepository;
 
-	public List<Student> searchStudents() {
-	        return null;
-	}
+	public List<Student> searchStudents(
+	        String firstName,
+	        String lastName,
+	        String lastNameStartsWith,
+	        Integer year,
+	        String city,
+	        String universityName
+	    ) {
+	        Student probe = new Student();
+	        probe.setFirstName(firstName);
+	        probe.setLastName(lastName);
+	        probe.setYear(year);
+	        probe.setCity(city);
+	        
+	        University university = new University();
+	        university.setName(universityName);
+	        probe.setUniversity(university);
+
+	        ExampleMatcher matcher = ExampleMatcher.matching()
+	            .withMatcher("lastName", m -> m.startsWith())
+	            .withMatcher("university.name", m->m.startsWith());
+
+	        Example<Student> example = Example.of(probe, matcher);
+	        List<Student> results = studentRepository.findAll(example);
+
+
+	        return results;
+	    }
 }
